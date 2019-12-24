8f9d241c62e45cfed37a6254c0efc20116fd9536
Niseema Pachikara <niseemapachikara@NP-MaK.local>
2019-12-24 00:38:41 -0800

    my second commit - a python file

---
+++ hello.py
@@ -0,0 +1,14 @@
+def my_function(a):	
+	return a**2
+
+print("This is a trial program")
+a = 4
+b = 5
+
+print("What is a + b = ?")
+print("a = {}".format(a))
+print("b = {}".format(b))
+print("a + b = {}".format(a + b))
+
+
+print("func(10) = {}".format(my_function(10)))
